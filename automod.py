9073571693203f01d0febd6b2d0d2054ac6c2b72
umm-maybe <91687904+umm-maybe@users.noreply.github.com>
2022-11-27 06:44:14 -0500

    Add negative keyword list

--- automod.py
+++ automod.py
@@ -28,33 +28,45 @@ mastodon = Mastodon(
 cuda_available = torch.cuda.is_available()
 detox = Detoxify('unbiased-small', device='cuda' if cuda_available else 'cpu')
 
+def bad_keyword(text, negative_keywords):
+    for keyword in negative_keywords:
+        if text.find(keyword) > -1:
+            return True
+    return False
+
 class Listener(StreamListener):
     def on_update(self, status):
+        report = False
         toot_html = status['content']
         toot_soup = BeautifulSoup(toot_html)
         toot_text = toot_soup.get_text()
         print(toot_text)
-        lang_code = langid.classify(toot_text)[0]
-        if lang_code=='en':
-            # use Detoxify
-            scores = detox.predict(toot_text)
+        if config['keywords'] and bad_keyword(toot_text,config['keywords']):
+            report = True
         else:
-            # no classification model available - pass
-            scores = None
-        if scores:
-            for attribute in config['thresholds'].keys():
-                score = scores[attribute]
-                print(f"{attribute}: {score}")
-                if score > config['thresholds'][attribute]:
-                     account_id = status['account']['id']
-                     status_ids = [status['id'],]
-                     mastodon.report(account_id,status_ids,comment="This report was made automatically.")
-                     mastodon.status_post(
-                         status="This status update has been reported to admins for toxic language.",
-                         in_reply_to_id=status['id'],
-                         visibility="private"
-                     )
-                     break # no need to continue checking
+            lang_code = langid.classify(toot_text)[0]
+            if lang_code=='en':
+                # use Detoxify
+                scores = detox.predict(toot_text)
+            else:
+                # no classification model available - pass
+                scores = None
+            if scores:
+                for attribute in config['thresholds'].keys():
+                    score = scores[attribute]
+                    print(f"{attribute}: {score}")
+                    if score > config['thresholds'][attribute]:
+                        report = True
+                        break # no need to continue checking
+        if report==True:
+             account_id = status['account']['id']
+             status_ids = [status['id'],]
+             mastodon.report(account_id,status_ids,comment="This report was made automatically.")
+             mastodon.status_post(
+                 status="This status update has been reported to admins for toxic language.",
+                 in_reply_to_id=status['id'],
+                 visibility="private"
+             )
 
 listener = Listener()
 mastodon.stream_local(listener)
